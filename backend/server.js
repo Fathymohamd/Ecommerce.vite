72e05f7ab3d43e1de2bdeecae94ded5162f38ba2
fathy mohamed <fathymohamedfathy299@gmail.com>
2026-07-15 21:19:26 +0300

    tt

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ app.get("/", async (req, res) => {
   res.send("Backend is running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =  process.env.PORT  || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
